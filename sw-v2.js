33ac30df54f9857c3500ba74484a80c898485809
KUROROSUKE <161828247+KUROROSUKE@users.noreply.github.com>
2025-05-17 15:05:31 +0900

    Update and rename sw.js to sw-v2.js

--- sw-v2.js
+++ sw-v2.js
@@ -1,9 +1,8 @@
 const CACHE_NAME = 'my-game-cache-v1';
 const urlsToCache = [
   'https://kurorosuke.github.io/PEB3AI/Code/game.min.html',
-  'https://kurorosuke.github.io/PEB3AI/Code/game.min.css',
-  'https://kurorosuke.github.io/PEB3AI/Code/game.min.js',
-  'https://kurorosuke.github.io/PEB3AI/images/start_screen_mobile.webp',
+  'https://kurorosuke.github.io/PEB3AI/Code/game-v2.min.css',
+  'https://kurorosuke.github.io/PEB3AI/Code/game-v8.min.js'
 ];
 
 // インストール
